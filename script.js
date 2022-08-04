f182b0d81e8df62c156c980fc23b756e9f9f2a8a
naglec13 <naglec13@gmail.com>
2022-08-04 19:10:52 +0300

    add background layers in script.js and create simple animation function

--- script.js
+++ script.js
@@ -1,106 +1,22 @@
-let playerState = "fall";
-const dropdown = document.getElementById("animations");
-dropdown.addEventListener("change", function (e) {
-  playerState = e.target.value;
-});
-
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = (canvas.width = 600);
-const CANVAS_HEIGHT = (canvas.height = 600);
+const CANVAS_WIDTH = (canvas.width = 800);
+const CANVAS_HEIGHT = (canvas.height = 700);
+let gameSpeed = 5;
 
-const playerImage = new Image();
-playerImage.src = "shadow_dog.png";
-const spriteWidth = 575;
-const spriteHeight = 523;
-let positionX = 575;
-let positionY = 523;
-let frameX = 0;
-let frameY = 3;
-let gameFrame = 0;
-const staggerFrames = 5;
-const spriteAnimations = [];
-const animationStates = [
-  {
-    name: "idle",
-    frames: 7,
-  },
-  {
-    name: "jump",
-    frames: 7,
-  },
-  {
-    name: "fall",
-    frames: 7,
-  },
-  {
-    name: "run",
-    frames: 9,
-  },
-  {
-    name: "dizzy",
-    frames: 11,
-  },
-  {
-    name: "sit",
-    frames: 5,
-  },
-  {
-    name: "roll",
-    frames: 7,
-  },
-  {
-    name: "bite",
-    frames: 7,
-  },
-  {
-    name: "ko",
-    frames: 12,
-  },
-  {
-    name: "getHit",
-    frames: 4,
-  },
-];
-
-animationStates.forEach((state, index) => {
-  let frames = {
-    loc: [],
-  };
-  for (j = 0; j < state.frames; j++) {
-    frames.loc.push({ x: positionX * j, y: positionY * index });
-  }
-  spriteAnimations[state.name] = frames;
-});
-
-console.log("spriteAnimations: ", spriteAnimations);
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "backgroundLayers/layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "backgroundLayers/layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "backgroundLayers/layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "backgroundLayers/layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "backgroundLayers/layer-5.png";
 
 function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position =
-    Math.floor(gameFrame / staggerFrames) %
-    spriteAnimations[playerState].loc.length;
-  frameX = position * spriteWidth;
-  frameY = spriteAnimations[playerState].loc[position].y;
-  ctx.drawImage(
-    playerImage,
-    frameX,
-    frameY,
-    spriteWidth,
-    spriteHeight,
-    0,
-    0,
-    spriteWidth,
-    spriteHeight
-  );
-
-  // ---one way to animate spritesheet---
-  // if (gameFrame % staggerFrames == 0) {
-  //   if (frameX < 10) frameX++;
-  //   else frameX = 0;
-  // }
-
-  gameFrame++;
+  ctx.drawImage(backgroundLayer4, 0, 0);
   requestAnimationFrame(animate);
 }
 
