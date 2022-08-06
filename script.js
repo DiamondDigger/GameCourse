5293ee61c3fe3705f1f1aae98683639c20303e29
naglec13 <naglec13@gmail.com>
2022-08-06 23:10:09 +0300

    add another animate function with class representation of layers

--- script.js
+++ script.js
@@ -17,6 +17,47 @@ backgroundLayer4.src = "backgroundLayers/layer-4.png";
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = "backgroundLayers/layer-5.png";
 
+class Layer {
+  constructor(image, speedModifier) {
+    this.width = 2400;
+    this.height = CANVAS_HEIGHT;
+    this.x = 0;
+    this.x2 = this.width;
+    this.y = 0;
+    this.image = image;
+    this.gameSpeed = gameSpeed;
+    this.speedModifier = speedModifier;
+    this.speed = this.gameSpeed * this.speedModifier;
+  }
+  update() {
+    this.gameSpeed = gameSpeed;
+    this.x < -this.width ? (this.x = 0) : (this.x -= this.speed);
+    this.x2 < 0 ? (this.x2 = this.width) : (this.x2 -= this.speed);
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x, 0);
+    ctx.drawImage(this.image, this.x2, 0);
+  }
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.1);
+const layer2 = new Layer(backgroundLayer2, 0.2);
+const layer3 = new Layer(backgroundLayer3, 0.3);
+const layer4 = new Layer(backgroundLayer4, 0.4);
+const layer5 = new Layer(backgroundLayer5, 0.7);
+
+const layerContainer = [layer1, layer2, layer3, layer4, layer5];
+
+function animateLayers() {
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  layerContainer.forEach((object) => {
+    object.update();
+    object.draw();
+  });
+  requestAnimationFrame(animateLayers);
+}
+
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   ctx.drawImage(backgroundLayer4, x, 0);
@@ -28,4 +69,6 @@ function animate() {
   requestAnimationFrame(animate);
 }
 
-animate();
+animateLayers();
+
+// animate();
